3c41e411e0bb2badfed0fbbe30245cf490b9ffcc
juanfran <juan_barrios_1995@hotmail.com>
2025-02-14 18:01:47 -0300

    ProyectoFinal-Barrios

--- JavaScript/scripts.js
+++ JavaScript/scripts.js
@@ -1,15 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
-    // sirve para crear el contenido principal
     const app = document.getElementById('aplicacion');
     if (!app) {
         console.error('Elemento con id "app" no encontrado en el DOM.');
         return;
     }
-   
+
     const container = document.createElement('div');
     container.classList.add('container');
 
-    // Historial de connversiones
     const historyContainer = document.createElement('div');
     historyContainer.classList.add('history-container');
     const historyTitle = document.createElement('h2');
@@ -19,25 +17,20 @@ document.addEventListener('DOMContentLoaded', () => {
     historyList.setAttribute('id', 'historyList');
     historyContainer.appendChild(historyList);
 
-    // Boton que vacia el LocalStorage de las ultimas 5 conversiones
     const clearHistoryButton = document.createElement('button');
     clearHistoryButton.textContent = 'Limpiar historial';
     clearHistoryButton.setAttribute('id', 'clearHistoryButton');
     clearHistoryButton.classList.add('clear-history-button');
     historyContainer.appendChild(clearHistoryButton);
-
     app.appendChild(historyContainer);
 
-    // Titulo del convertidor
     const title = document.createElement('h1');
     title.textContent = 'Convertidor de Monedas';
     container.appendChild(title);
 
-    // Formulario
     const converter = document.createElement('div');
     converter.classList.add('converter');
-    
-    // Inputs
+
     const amountLabel = document.createElement('label');
     amountLabel.setAttribute('for', 'amount');
     amountLabel.textContent = 'Cantidad:';
@@ -45,42 +38,36 @@ document.addEventListener('DOMContentLoaded', () => {
     amountInput.setAttribute('type', 'number');
     amountInput.setAttribute('id', 'amount');
     amountInput.setAttribute('placeholder', 'Ingrese monto a convertir');
-    
-    // Select con los distintos tipos de monedas (ARG - USD - EUR)
+
     const fromLabel = document.createElement('label');
     fromLabel.setAttribute('for', 'from');
     fromLabel.textContent = 'De:';
     const fromSelect = document.createElement('select');
     fromSelect.setAttribute('id', 'from');
     const currencyOptions = ['ARS', 'USD', 'EUR'];
-    
     currencyOptions.forEach(optionValue => {
         const option = document.createElement('option');
         option.setAttribute('value', optionValue);
         option.textContent = optionValue;
         fromSelect.appendChild(option);
     });
-    
-    // Select para el tipo de moneda de cambio
+
     const toLabel = document.createElement('label');
     toLabel.setAttribute('for', 'to');
     toLabel.textContent = 'A:';
     const toSelect = document.createElement('select');
     toSelect.setAttribute('id', 'to');
-    
     currencyOptions.forEach(optionValue => {
         const option = document.createElement('option');
         option.setAttribute('value', optionValue);
         option.textContent = optionValue;
         toSelect.appendChild(option);
     });
-    
-    // Boton de conversion
+
     const convertButton = document.createElement('button');
     convertButton.textContent = 'Convertir';
     convertButton.setAttribute('id', 'convertButton');
-    
-    // Agrega elementos al formulario
+
     converter.appendChild(amountLabel);
     converter.appendChild(amountInput);
     converter.appendChild(fromLabel);
@@ -88,14 +75,10 @@ document.addEventListener('DOMContentLoaded', () => {
     converter.appendChild(toLabel);
     converter.appendChild(toSelect);
     converter.appendChild(convertButton);
-    
-    // Al formulario lo agrega al contenedor principal
     container.appendChild(converter);
     app.appendChild(container);
 
     let exchangeRates = null;
-    
-    //carga el archivo Json con los valores pre-establecidos
     fetch('./Json/valores.json')
         .then(response => {
             if (!response.ok) {
@@ -115,18 +98,15 @@ document.addEventListener('DOMContentLoaded', () => {
             });
         });
 
-    // Carga el historial
     function loadHistory() {
         const history = JSON.parse(localStorage.getItem('conversionHistory')) || [];
-        historyList.innerHTML = ''; // vacia la lista
+        historyList.innerHTML = '';
         history.forEach(item => {
             const listItem = document.createElement('li');
             const currencyIcon = document.createElement('img');
             currencyIcon.style.width = '20px';
             currencyIcon.style.height = '20px';
             currencyIcon.style.marginRight = '5px';
-
-            // agrega imagen según la moneda
             switch (item.to) {
                 case 'USD':
                     currencyIcon.src = 'https://upload.wikimedia.org/wikipedia/commons/a/a4/Flag_of_the_United_States.svg';
@@ -138,97 +118,59 @@ document.addEventListener('DOMContentLoaded', () => {
                     currencyIcon.src = 'https://upload.wikimedia.org/wikipedia/commons/b/b7/Flag_of_Europe.svg';
                     break;
             }
-
             listItem.appendChild(currencyIcon);
-            listItem.appendChild(document.createTextNode(`${item.amount} ${item.from} a ${item.to}: ${item.convertedAmount}`));
+            const formattedAmount = new Intl.NumberFormat('es-ES', { minimumFractionDigits: 2 }).format(item.amount);
+            const formattedConvertedAmount = new Intl.NumberFormat('es-ES', { minimumFractionDigits: 2 }).format(item.convertedAmount);
+            listItem.appendChild(document.createTextNode(`${formattedAmount} ${item.from} a ${item.to}: ${formattedConvertedAmount}`));
             historyList.appendChild(listItem);
         });
     }
 
-    // Función para guardar en LocalStorage
     function saveToHistory(amount, from, to, convertedAmount) {
         let history = JSON.parse(localStorage.getItem('conversionHistory')) || [];
         const today = new Date().toISOString().split('T')[0];
-
-        // certifica que las conversiones sean del dia actual
         history = history.filter(item => item.date === today);
-
-        // agrega nueva conversion
-        history.push({ amount, from, to, convertedAmount, date: today });
-
-        // mantiene solo las últimas 5 conversiones
+        history.push({ amount: Number(amount), from, to, convertedAmount: Number(convertedAmount), date: today });
         if (history.length > 5) {
             history.shift();
         }
-
         localStorage.setItem('conversionHistory', JSON.stringify(history));
         loadHistory();
     }
 
-    // Realizar la conversion
     function convertCurrency() {
         const amount = parseFloat(amountInput.value);
         const fromCurrency = fromSelect.value;
         const toCurrency = toSelect.value;
-        
         if (isNaN(amount) || amount <= 0) {
-            Swal.fire({
-                icon: 'warning',
-                title: 'Monto inválido',
-                text: 'Por favor ingrese un monto válido.'
-            });
+            Swal.fire({ icon: 'warning', title: 'Monto inválido', text: 'Por favor ingrese un monto válido.' });
             return;
         }
-
         if (fromCurrency === toCurrency) {
-            Swal.fire({
-                icon: 'warning',
-                title: 'Monedas iguales',
-                text: 'Las monedas seleccionadas son iguales.'
-            });
+            Swal.fire({ icon: 'warning', title: 'Monedas iguales', text: 'Las monedas seleccionadas son iguales.' });
             return;
         }
-
         if (!exchangeRates) {
-            Swal.fire({
-                icon: 'error',
-                title: 'Error',
-                text: 'Las tasas de conversión aún no están disponibles.'
-            });
+            Swal.fire({ icon: 'error', title: 'Error', text: 'Las tasas de conversión aún no están disponibles.' });
             return;
         }
-
         if (exchangeRates[fromCurrency] && exchangeRates[fromCurrency][toCurrency]) {
             const conversionRate = exchangeRates[fromCurrency][toCurrency];
             const convertedAmount = amount * conversionRate;
-            Swal.fire({
-                icon: 'success',
-                title: 'Conversión exitosa',
-                text: `Resultado: ${convertedAmount.toFixed(2)} ${toCurrency}`
-            });
-            saveToHistory(amount, fromCurrency, toCurrency, convertedAmount.toFixed(2));
+            const formattedConvertedAmount = new Intl.NumberFormat('es-ES', { minimumFractionDigits: 2 }).format(convertedAmount);
+            Swal.fire({ icon: 'success', title: 'Conversión exitosa', text: `Resultado: ${formattedConvertedAmount} ${toCurrency}` });
+            saveToHistory(amount, fromCurrency, toCurrency, convertedAmount);
         } else {
-            Swal.fire({
-                icon: 'error',
-                title: 'Error',
-                text: 'No se pudo realizar la conversión. Intente más tarde.'
-            });
+            Swal.fire({ icon: 'error', title: 'Error', text: 'No se pudo realizar la conversión. Intente más tarde.' });
         }
     }
 
-    // Limpiar historial
     clearHistoryButton.addEventListener('click', () => {
         localStorage.removeItem('conversionHistory');
         loadHistory();
-        Swal.fire({
-            icon: 'info',
-            title: 'Historial eliminado',
-            text: 'El historial de conversiones se ha eliminado correctamente.'
-        });
+        Swal.fire({ icon: 'info', title: 'Historial eliminado', text: 'El historial de conversiones se ha eliminado correctamente.' });
     });
 
     convertButton.addEventListener('click', convertCurrency);
-
-    // Cargar historial al cargar la página
     loadHistory();
 });
